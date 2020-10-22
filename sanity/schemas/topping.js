c2c1dcf99925c58be97b0d9cfce6b3585a1e03b3
Alexis Araujo <ala1289@gmail.com>
2020-10-22 14:35:39 -0500

    Creando el contenido de los topings y vistas customizadas

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,31 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'What is the name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian Name',
+      type: 'boolean',
+      description: 'What is the name of the topping',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: { name: 'name', vegetarian: 'vegetarian' },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🥬' : ''}`,
+    }),
+  },
+};
