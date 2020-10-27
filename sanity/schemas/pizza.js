610cbbda6818841292759c667e51fe858bcc60ec
Alexis Araujo <ala1289@gmail.com>
2020-10-27 07:22:25 -0600

    Creando nuestras relaciones

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -37,5 +37,32 @@ export default {
       validation: (Rule) => Rule.min(1000),
       //Todo: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepae: ({ title, media, ...toppings }) => {
+      // 1.- Filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      console.log('Stay');
+      // 2.- Return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
