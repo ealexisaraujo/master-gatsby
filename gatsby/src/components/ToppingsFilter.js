3760baf8b000b79bd6bdd621489173ff0396a99e
Alexis Araujo <ala1289@gmail.com>
2021-03-03 08:09:51 -0600

    Creando el componente de filtros de toppings

---
+++ gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import styled from 'styled-components';
+
+export default function ToppingsFilter() {
+  // Get a list of all the toppings
+  // Get a list of all the Pizzas with their toppings
+  // Count how many pizzas are in each topping
+  // Loop over the list of toppings and display the topping and the count of pizzas in that topping
+  // Link it up.. ...  . . .
+  return (
+    <div>
+      <p>Toppings!</p>
+    </div>
+  );
+}
