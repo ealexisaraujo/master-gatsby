000145cae49c5e0d6851c63900b1ac9f9c5537c8
Alexis Araujo <ala1289@gmail.com>
2021-03-03 11:52:58 -0600

    Creando un template

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,3 +1,12 @@
+import path from 'path';
+
+async function turnPizzasIntoPages(params) {
+  // 1. Get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  // 2. Query all pizzas
+  // 3. Loop over each pizza and create a page for that pizza
+}
+
 export async function createPages(params) {
   // Create pages dynamically
   // 1. Pizzas
---
+++ gatsby/src/templates/Pizza.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+export default function SinglePizzaPage() {
+  return <p>Single Pizza!!!</p>;
+}
